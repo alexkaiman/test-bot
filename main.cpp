d7e341929d3d08a48e8d7410005aada0727b8327
Alex Kaiman <alexander.kaiman@gmail.com>
2024-08-06 10:58:54 +0500

    Добавил использование аргументов строки

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <ctime>
 #include <thread>
+#include <string>
+#include <getopt.h>
 #include "telegram.h"
 #include "logger.h"
 #include "config.h"
 #include "emoji.h" // взято https://github.com/99x/emojicpp
 
 using namespace std::chrono_literals;
+
+// Глобальная переменная для хранения пользовательского сообщения
+std::string customMessage = "Тестовое сообщение";
 
 // Функция для отправки сообщения
 void sendmessage() {
@@ -16,7 +21,7 @@ void sendmessage() {
     TelegramBot bot(Config::TELEGRAM_TOKEN.c_str());
     
     // Формируем сообщение с эмодзи
-    std::string message = emojicpp::emojize(":bangbang:Тестовое сообщение:bangbang: :smile: \n");
+    std::string message = emojicpp::emojize(":bangbang:" + customMessage + ":bangbang: :smile: \n");
     message += emojicpp::emojize(":dragon: :smile: \n"); 
 
     // Отправляем сообщение
@@ -27,14 +32,57 @@ void sendmessage() {
     }
 }
 
-int main() {
-    // Загружаем конфигурацию из файла config.ini
-    if (!Config::load("config.ini")) {
+void printUsage(const char* programName) {
+    std::cout << "Использование: " << programName << " [опции]\n"
+              << "Опции:\n"
+              << "  -l, --log <файл>   Путь к лог-файлу\n"
+              << "  -c, --conf <файл>  Путь к конфигурационному файлу\n"
+              << "  -m, --mess <текст> Пользовательское сообщение\n"
+              << "  -h, --help         Показать эту справку\n";
+}
+
+int main(int argc, char* argv[]) {
+    std::string logFile = "/var/log/telegram_bot.log";
+    std::string configFile = "config.ini";
+
+    static struct option long_options[] = {
+        {"log", required_argument, 0, 'l'},
+        {"conf", required_argument, 0, 'c'},
+        {"mess", required_argument, 0, 'm'},
+        {"help", no_argument, 0, 'h'},
+        {0, 0, 0, 0}
+    };
+
+    int opt;
+    int option_index = 0;
+    while ((opt = getopt_long(argc, argv, "l:c:m:h", long_options, &option_index)) != -1) {
+        switch (opt) {
+            case 'l':
+                logFile = optarg;
+                break;
+            case 'c':
+                configFile = optarg;
+                break;
+            case 'm':
+                customMessage = optarg;
+                break;
+            case 'h':
+                printUsage(argv[0]);
+                return 0;
+            default:
+                printUsage(argv[0]);
+                return 1;
+        }
+    }
+
+    // Загружаем конфигурацию из файла
+    if (!Config::load(configFile.c_str())) {
+        std::cerr << "Ошибка при загрузке конфигурационного файла: " << configFile << std::endl;
         return 1;
     }
     
-    // Инициализируем логгер, не забываем про права на telegram_bot.log
-    Logger::init("/var/log/telegram_bot.log");
+    // Инициализируем логгер
+    Logger::init(logFile.c_str());
     Logger::info("Бот запускается");
 
     try {
